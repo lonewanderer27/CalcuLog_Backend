61d11d755cd270483ca108604494b5a221aa144b
lonewanderer27 <adrianejames27@gmail.com>
2023-03-06 11:35:27 +0800

    add initial routes for PE and TM

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
 import uvicorn
 
-app = FastAPI()
+app = FastAPI(title="CalcuLog Backend")
+
+
+class PEvalues(BaseModel):
+    trueValue: str
+    approxValue: float
+    roundingchopping: str
+    numDigits: float
+
+
+class TMvalues(BaseModel):
+    function: str
+    point: float
+    nthDegree: float
+
 
 @app.get("/")
-async def root():
-    return "Hello World"
+async def root(request: Request):
+    return f"CalcuLog Backend | Go to this URL for docs: {request.url._url}docs"
+
+
+@app.post("/propagationError")
+async def propagation_error(PEvalues: PEvalues):
+    return "Propagation Error"
+
+
+@app.post("/taylorMaclaurin")
+async def taylor_maclaurin(TMvalues: TMvalues):
+    return "Taylor Maclaurin"
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0")
