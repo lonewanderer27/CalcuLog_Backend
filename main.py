c289c1f00d826c449cd7e11a009793d06d4e897f
lonewanderer27 <adrianejames27@gmail.com>
2023-03-06 10:48:46 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from typing import Union
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return "Hello World"
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0")
