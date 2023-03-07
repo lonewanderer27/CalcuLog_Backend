7267caa38a13c7e46a5051699b3ed703db8d0b48
lonewanderer27 <adrianejames27@gmail.com>
2023-03-07 12:42:50 +0800

    add propagation_error solver

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Union
 from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
+from solvers import wolfram, compute_absolute_error, parse_roundingchopping
+
 import uvicorn
 
 app = FastAPI(title="CalcuLog Backend")
@@ -12,6 +14,13 @@ class PEvalues(BaseModel):
     approxValue: float
     roundingchopping: str
     numDigits: float
+
+
+class PEresult(BaseModel):
+    absolute_error: float
+    absolute_percentage_error: float
+    relative_error: float
+    relative_percentage_error: float
 
 
 class TMvalues(BaseModel):
@@ -25,12 +34,47 @@ async def root(request: Request):
     return f"CalcuLog Backend | Go to this URL for docs: {request.url._url}docs"
 
 
-@app.post("/propagationError")
-async def propagation_error(PEvalues: PEvalues):
-    return "Propagation Error"
+@app.get("/pe")
+async def propagation_error(
+    trueValue: str,
+    approxValue: str,
+    roundingchopping: str,
+    numDigits: int
+):
+    try:
+        # if only number is given, then we assign it directly as the true value
+        true_value_result = float(trueValue)
+    except ValueError:
+        # otherwise, if it's an equation we compute the true value
+        true_value_result = wolfram(trueValue)
+        print("true_value result:", true_value_result)
+
+    try:
+        # if only number is given, then we assign it directly as the approx value
+        approx_value_result = float(approxValue)
+    except ValueError:
+        # otherwise, if it's an equation we compute the approx value
+        approx_value_result = wolfram(approxValue)
+        print("approx_value result:", approx_value_result)
+
+    # compute absolute_error
+    [ab_error, ab_error_percentage] = compute_absolute_error(
+        true_value_result, approx_value_result)
+    print("ab_error:", ab_error)
+    print("ab_error_percentage:", ab_error_percentage)
+
+    ab_error = parse_roundingchopping(ab_error, roundingchopping, numDigits)
+    ab_error_percentage = parse_roundingchopping(
+        ab_error_percentage, roundingchopping, numDigits)
+
+    # return the result as json object
+    return {
+        "absolute_error": ab_error,
+        "absolute_error_percentage": ab_error_percentage,
+    }
 
 
-@app.post("/taylorMaclaurin")
+@app.post("/tm")
 async def taylor_maclaurin(TMvalues: TMvalues):
     return "Taylor Maclaurin"
 
---
+++ solvers.py
@@ -0,0 +1,45 @@
+import requests
+import math
+from constants import WOLFRAM_APPID
+
+
+def truncate(number, digits) -> float:
+    # Improve accuracy with floating point operations, to avoid truncate(16.4, 2) = 16.39 or truncate(-1.13, 2) = -1.12
+    nbDecimals = len(str(number).split('.')[1])
+    if nbDecimals <= digits:
+        return number
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
+
+
+def wolfram(equation: str) -> float:
+    print("equation:", equation)
+
+    # replace all four operations with a literal word
+    equation_convd = equation.replace(
+        "+", " add ").replace("-", " minus ").replace("*", " times ").replace("/", " divide ")
+
+    print("input_converted:", equation_convd)
+
+    response = requests.get("https://api.wolframalpha.com/v2/query", params={
+        "appid": WOLFRAM_APPID,
+        "input": equation_convd,
+        "format": "plaintext",
+        "output": "json"
+    })
+    print(response.url)
+
+    return float(response.json()["queryresult"]["pods"][1]["subpods"][0]["plaintext"])
+
+
+def compute_absolute_error(true_value: float, approx_value: float) -> list[float]:
+    error = abs(true_value - approx_value)
+    error_percentage = abs(error / true_value) * 100
+    return [error, error_percentage]
+
+
+def parse_roundingchopping(value: float, roundingchopping: str, numDigits: int):
+    if roundingchopping == "chopping":
+        return truncate(value, numDigits)
+    else:
+        return round(value, numDigits)
